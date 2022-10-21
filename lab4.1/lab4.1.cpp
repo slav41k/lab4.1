072d0fa497addd7470acbc154769fe0e8b22f08f
slav41k <yaroslav.teslitskyi.ri.2022@lpnu.ua>
2022-10-21 14:50:50 +0300

    lab4.1

---
+++ lab4.1/lab4.1.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int N, i;
+	double S;
+	cout << "N = "; cin >> N;
+	S = 0;
+	i = 1;
+	while (i <= N)
+	{
+		S += (sin(cos(i)))/(1 + cos(i) * cos(i));
+		i++;
+	}
+	cout << S << endl;
+		S = 0;
+	i = 1;
+	do {
+		{
+			if (N == 0)
+			{
+				S = 0;
+			}
+		}
+		S += (sin(cos(i))) / (1 + cos(i) * cos(i));
+		i++;
+	} while (i <= N);
+	cout << S << endl;
+	S = 0;
+	for (i = 1; i <= N; i++)
+	{
+		S += (sin(cos(i))) / (1 + cos(i) * cos(i));
+	}
+	cout << S << endl;
+	S = 0;
+	for (i = N; i >= 1; i--)
+	{
+		S += (sin(cos(i))) / (1 + cos(i) * cos(i));
+	}
+	cout << S << endl;
+	return 0;
+}
